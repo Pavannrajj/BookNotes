b7852253ba7384c0fc0c7e9093fe1eb2dceb6d39
pavanfromlaggere <pavannrajjj@gmail.com>
2025-07-31 07:16:43 +0530

    Clean up 2

--- databaseFetch.js
+++ databaseFetch.js
@@ -5,13 +5,20 @@ import dotenv from "dotenv";
 
 dotenv.config();  
 
+// const db = new pg.Client({
+//   user: process.env.PGUSER,
+//   host: process.env.PGHOST,
+//   database: process.env.PGDATABASE,
+//   password: process.env.PGPASSWORD,
+//   port: process.env.PGPORT,
+//     ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
 const db = new pg.Client({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
-    ssl: {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
     rejectUnauthorized: false
   }
 });
